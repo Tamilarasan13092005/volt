e602648bbe0db09e2e9c1427bc9e5c0f52cab17b
tamil <tamil246arasan@gmail.com>
2026-06-12 19:24:27 +0530

    Rebuild web with latest fixes

--- volunteersync/build/web/flutter_bootstrap.js
+++ volunteersync/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "345194412" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1215041142" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
